20a67fff8593b57fbf1472a621942a95b84fa30e
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-07-31 19:50:46 +0000

    Adding the downloadImageByURL function

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,61 @@
 var request = require('request');
+var fs      = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "rayhaneh";
+var GITHUB_TOKEN = "50b6098a754070bbc17f0bdc980689b70d3a6e82";
+
+
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+
+  request(options, function (error, response, body) {
+    var info = null
+    if (!error && response.statusCode == 200) {
+      info = JSON.parse(body);
+    }
+    cb (error,info)
+  });
+
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+function downloadImageByURL(url, filePath) {
+request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .pipe(fs.createWriteStream(filePath))
+       .on('finish',function () {
+        console.log('Github avatar saved to ' + filePath)
+       });
+
+}
+
+
+
+
+
+
+
+getRepoContributors(process.argv[2], process.argv[3], function(error, result) {
+  if (!error){
+    result.forEach(function (userData){
+      var filePath = userData.avatar_url.split("/")[4].split("?")[0] + '.jpg';
+      downloadImageByURL(userData.avatar_url,filePath)
+    })
+  } else {
+    console.log(error)
+  }
 });
+
