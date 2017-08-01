b81d2104a706855d3c3d05055887512398dcaf95
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-01 14:24:08 +0000

    Adding the first version of the recommand function

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,15 @@
 // Required Modules
 var request = require('request');
 var fs      = require('fs');
-var dotenv  = require('dotenv').config({path: './.env'});
+var dotenv  = require('dotenv').config({path: '.env'});
+var getRepoContributors = require('./getRepoContributors');
 
 // Initializing
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER   = process.env.GITHUB_USER;
 var GITHUB_TOKEN  = process.env.GITHUB_TOKEN;
+
 
 var dir = "./avatars/"
 
@@ -57,62 +59,26 @@ if (process.argv.length !== 4 ){
 
 
 
-
-
-
-
-// A function to get the contributors of a given github repo
-function getRepoContributors(repoOwner, repoName, cb) {
-
-  var requestURL = `https://'${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-
-  var options = {
-    url: requestURL,
-    headers: {
-      'User-Agent': 'GitHub Avatar Downloader - Student Project'
-    }
-  };
-
-  request(options, function (error, response, body) {
-    var info = null;
-    // Parse the response to info if the transfer was successful
-    if (!error && response.statusCode === 200) {
-      info = JSON.parse(body);
-
-    // Check if the error was because the owner or the repo do not exist
-    } else if (response.statusCode === 404) {
-
-      error = 'The repo or owner does not exists (Status Code: 400)';
-
-    // Check if the error was because the .env file contains incorrect credentials
-    } else  if(reponse.statusCode === 401) {
-      error = 'The .env file contains incorrect credentials';
-
-    // For all cases of error
-    } else {
-      error = error + response.statusCode;
-    }
-    cb (error,info);
-  });
-
-}
-
-
 // A function to download an image from a given URL
 function downloadImageByURL(url, filePath) {
-request.get(url)
-       .on('error', function (err) {
-         throw err;
-       })
-       .pipe(fs.createWriteStream(filePath))
-       .on('finish',function () {
-        console.log('Github avatar saved to ' + filePath);
-       });
-
+  request.get(url)
+  .on('error', function (err) {
+    throw err;
+  })
+  .pipe(fs.createWriteStream(filePath))
+  .on('finish',function () {
+    console.log('Github avatar saved to ' + filePath);
+  });
 }
-// Exporting the downloadImageByURL function for testing
-module.exports = downloadImageByURL;
 
+
+
+
+
+// Exporting function for testing and using in the recommand.js
+module.exports = {
+  downloadImageByURL : downloadImageByURL,
+}
 
 
 
